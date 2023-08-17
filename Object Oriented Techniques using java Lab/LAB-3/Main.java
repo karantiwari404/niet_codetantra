02db1dba587b6f3720bee49ea3d5fe814d5472c8
Karan <124139172+iamVampire4@users.noreply.github.com>
2023-08-17 22:27:53 +0530

    Add files via upload

---
+++ Object Oriented Techniques using java Lab/LAB-3/Main.java
@@ -0,0 +1,12 @@
+package q116;
+import java.util.Scanner;
+public class Main{
+    public static void main(Stirng[] args){
+        System.out.print("Enter a string: ");
+        Scanner sn=new Scanner(System.in);
+        String a=sn.nextLine();
+        sn.close();
+        System.out.println("The entered string is: "+a);
+    }
+}
+//karan tiwari
---
+++ Object Oriented Techniques using java Lab/LAB-7/Division.java
@@ -0,0 +1,13 @@
+package q11329;
+public class Division{
+	public static void main(String args[]){
+		int number1 = Integer.parseInt(args[0]);
+		int number2 = Integer.parseInt(args[1]);
+try{
+	int d = number1/number2;
+	System.out.println("Result = "+d);
+}
+catch(ArithmeticException e){
+	System.out.println("Exception caught : divide by zero occurred");
+}}}
+//karan tiwari
